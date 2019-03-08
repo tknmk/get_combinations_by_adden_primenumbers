04c6ba4387c1abed2cfdadf689c03f6ff73b27f3
takanami.kodai <kohdai.takanami@gmail.com>
2019-03-08 11:49:28 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Get all combinations of prime numbers with a given sum 
+
+First, get the prime numbers from input.
+
+まず、素数を取得します。
+
+Second, add the result, prime numbers so that the number is an input number(base_num).
+
+次にその取得した結果の素数を、入力した数値まで足し算する。
+
+finally, get all combinations.
+
+最終的に、指定した数値の素数で、指定した数値が合計になるような組み合わせを出力します。
+
+
+### Usage
+
+1. git clone
+2. Edit this code as you want to.
+3. run!
+
---
+++ find_prime_num.py
@@ -0,0 +1,52 @@
+import sys
+import numpy as np
+import itertools
+import pprint
+
+# input a base number from console.
+argvs = sys.argv
+pn = int(argvs[1])
+
+
+def main():
+    # print Prime Numbers and count them
+    pn_nums = get_prime_nums(pn)
+    pn_count = len(pn_nums)
+    print(pn_nums)
+    print('Prime numbers count: {0}'.format(pn_count))
+
+    # print combinations
+    pairs = get_combinations(pn_nums, pn)
+    pairs_count = len(pairs)
+    pprint.pprint(pairs)
+    print('Combinations count: {0}'.format(pairs_count))
+
+def get_prime_nums(pn):
+
+    max = int(np.sqrt(pn))
+    seachList = [i for i in range(2,pn+1)]
+    primeNum = []
+    while seachList[0] <= max:
+        primeNum.append(seachList[0])
+        tmp = seachList[0]
+        seachList = [i for i in seachList if i % tmp != 0]
+    primeNum.extend(seachList)
+    return primeNum
+
+
+def get_combinations(nums, base_num):
+
+    #result = []
+    result = [seq for i in range(len(nums), 0, -1) for seq in itertools.combinations(nums, i) if sum(seq) == base_num]
+    return result
+
+    #for i in range(len(nums), 0, -1):
+    #        for seq in itertools.product(nums, repeat=i):
+    #            if sum(seq) == base_num:
+    #                result.append(seq)
+    #    return result
+
+
+
+if __name__ == '__main__':
+    main()
